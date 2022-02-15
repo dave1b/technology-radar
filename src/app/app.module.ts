395ff735a239885bb2f572a0c6e730621e890ddb
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-15 17:18:24 +0100

    import of new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TechnologyPanelComponent } from './technology-panel/technology-panel.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { PanelContainerComponent } from './panel-container/panel-container.component';
+import { AddTechnologyComponent } from './add-technology/add-technology.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TechnologyPanelComponent
+    TechnologyPanelComponent,
+    DetailViewComponent,
+    PanelContainerComponent,
+    AddTechnologyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
