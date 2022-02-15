6ce9b8065e2400469aef18dcbac75e56312fdb8a
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-15 17:22:10 +0100

    call of function instead of member variable in view

--- src/app/technology-panel/technology-panel.component.ts
+++ src/app/technology-panel/technology-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from 'express';
 import { empty, isEmpty } from 'rxjs';
 import { TechDataService } from '../tech-data.service';
 import { Technology } from '../technology';
@@ -13,40 +14,29 @@ export class TechnologyPanelComponent implements OnInit {
 
   @Input() category: string = "";
   arrayOfTechnology?: Array<Technology>;
-  assessArray?: Array<Technology>;
-  trialArray?: Array<Technology>;
-  adoptArray?: Array<Technology>;
-  holdArray?: Array<Technology>;
-  
+ 
 
-  constructor(private techDataService: TechDataService) { 
+
+  constructor(private techDataService: TechDataService) {
 
   }
-  
-  
+
   ngOnInit(): void {
-    console.log("init")
-    this.techDataService.getAllByCategory(this.category).subscribe( data => {
+    this.techDataService.getAllByCategory(this.category).subscribe(data => {
       this.arrayOfTechnology = data;
-      console.log(data);
-      console.log("hello " +this.arrayOfTechnology);
-    
     });
   }
 
-  getAllTechnologyByStatus(status: string) :Array<Technology>{
-    if(!typeof(this.arrayOfTechnology)) {
-      var techArray: Array<Technology> = [];
-      for(var technology of this.arrayOfTechnology!){
-        if(technology.status == status){
+  getAllTechnologyByStatus(status: string): Array<Technology> {
+    var techArray: Array<Technology> = [];
+    if(!(typeof(this.arrayOfTechnology) == "undefined")){
+
+      for (var technology of this.arrayOfTechnology!) {
+        if (technology.status == status) {
           techArray.push(technology);
         }
       }
-      return techArray;
-    } else {
-      return [{  name: "empty"}];
     }
+    return techArray;
   }
-
-
 }
