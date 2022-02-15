7fa9e820d32841fae6d0ee394965c624d3a079b9
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-15 17:18:51 +0100

    new method -> getByName

--- src/app/tech-data.service.ts
+++ src/app/tech-data.service.ts
@@ -16,6 +16,11 @@ export class TechDataService {
   public getAllByCategory(technology: string) :Observable<[Technology]> {
     return this.http.get<[Technology]>(this.baseURL+"getAllByCategory/"+technology);
   }
+  
+  public getByName(name: string) :Observable<Technology> {
+    console.log("in get by name");
+    return this.http.get<Technology>(this.baseURL+"getByName/"+name);
+  }
 
   public changeStatusById(id: number, newStatus: string){
     return this.http.post(this.baseURL+id, newStatus);
@@ -25,9 +30,8 @@ export class TechDataService {
     return this.http.post(this.baseURL+"addTechnology", newTechnology);
   }
 
-  public changeTechnologyById(id: number, name?: string, category?: string, description?: string){
+  public changeTechnologyByName( name?: string, category?: string, description?: string){
     var obj = { 
-      id: id, //scalar value 
       name: name,  
       category: category,  
       description: description,  
