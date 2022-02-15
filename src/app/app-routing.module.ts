3c4a71bc2f43df382bd13a74e3db19f460877e3e
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-15 17:17:53 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTechnologyComponent } from './add-technology/add-technology.component';
+import { AppComponent } from './app.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { PanelContainerComponent } from './panel-container/panel-container.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'technologies/:name', component: DetailViewComponent },
+  { path: '', component: PanelContainerComponent },
+  { path: 'addTechnology', component: AddTechnologyComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
