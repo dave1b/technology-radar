00ae9e33a0413275a7a3854fee9dac2e0bc3bb2d
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-16 14:11:18 +0100

    another version

--- src/app/add-technology/add-technology.component.ts
+++ src/app/add-technology/add-technology.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { TechDataService } from '../tech-data.service';
 import { Technology } from '../technology';
 
@@ -10,15 +11,16 @@ import { Technology } from '../technology';
   styleUrls: ['./add-technology.component.scss']
 })
 export class AddTechnologyComponent implements OnInit {
-  myForm!: FormGroup;
+  adTechForm!: FormGroup;
   submitted = false;
 
 
-  constructor(private formBuilder: FormBuilder, private techDataService: TechDataService) { }
+  constructor(private formBuilder: FormBuilder, private techDataService: TechDataService, private router: Router) { }
 
   ngOnInit(): void {
-    this.myForm = this.formBuilder.group({
-      name: ['', Validators.required],
+    this.adTechForm = this.formBuilder.group({
+      
+      name: ['', Validators.required, Validators.minLength(6)],
       category: ['', Validators.required],
       status: ['', Validators.required],
       description: ['', Validators.required],
@@ -28,14 +30,15 @@ export class AddTechnologyComponent implements OnInit {
     });
   }
 
-  onSubmit(form: FormGroup) {
+  onSubmit() {
     this.submitted = true;
 
     // stop here if form is invalid
-    console.log('Valid?', form.valid); // true or false
-    if (this.myForm!.invalid) {
+    console.log('Valid?', this.adTechForm.valid); // true or false
+    if (this.adTechForm!.invalid) {
         return;
     }
+    /*
     console.log('Name', form.value.name);
     console.log('Category', form.value.category);
     console.log('Status', form.value.status);
@@ -43,16 +46,25 @@ export class AddTechnologyComponent implements OnInit {
     console.log('statusDescription', form.value.statusDescription); 
     console.log('author', form.value.author); 
     console.log('created', form.value.created); 
+    */
+    var date =  new Date();
+    var dateTimeStamp = date.toLocaleDateString() + " " + date.toLocaleTimeString();
     var newObject: Technology = {
-    name: form.value.name,
-    category: form.value.category,
-    status: form.value.status,
-    description: form.value.description,
-    statusDescription: form.value.statusDescription,
-    author: form.value.author,
-    created: (new Date()).toString(),
+    name: this.adTechForm.value.name,
+    category: this.adTechForm.value.category,
+    status: this.adTechForm.value.status,
+    description: this.adTechForm.value.description,
+    statusDescription: this.adTechForm.value.statusDescription,
+    author: this.adTechForm.value.author,
+    created: dateTimeStamp,
     }
+    //console.log(newObject);
     this.techDataService.addNewTechnology(newObject);
+    this.router.navigate(['']);
+}
+
+get getControl() {
+  return this.adTechForm.controls;
 }
 
 }
