34ccc4f602ac3dbc25736423baaf15573e8418a6
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-17 18:49:04 +0100

    new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule,FormsModule  } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TechnologyPanelComponent } from './technology-panel/technology-panel.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DetailViewComponent } from './detail-view/detail-view.component';
 import { PanelContainerComponent } from './panel-container/panel-container.component';
 import { AddTechnologyComponent } from './add-technology/add-technology.component';
+import { LoginComponent } from './login/login.component';
+import { UnpublishedComponent } from './unpublished/unpublished.component';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { TokenInterceptorService } from './token-interceptor.service';
+import { EditTechnologyComponent } from './edit-technology/edit-technology.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,10 @@ import { AddTechnologyComponent } from './add-technology/add-technology.componen
     TechnologyPanelComponent,
     DetailViewComponent,
     PanelContainerComponent,
-    AddTechnologyComponent
+    AddTechnologyComponent,
+    LoginComponent,
+    UnpublishedComponent,
+    EditTechnologyComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +36,11 @@ import { AddTechnologyComponent } from './add-technology/add-technology.componen
     ReactiveFormsModule,
     FormsModule
   ],
-  providers: [],
+  providers: [AuthGuard, AuthService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
