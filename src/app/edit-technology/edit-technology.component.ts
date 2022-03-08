e75c6b8203f2fe5bb8d3421a151ed35edaa43554
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-03-08 09:20:36 +0100

    added more input validation

--- src/app/edit-technology/edit-technology.component.ts
+++ src/app/edit-technology/edit-technology.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
@@ -27,9 +28,7 @@ export class EditTechnologyComponent implements OnInit {
     this.route.params.subscribe(
       (params: Params) => {
         this.name = params['name'];
-        console.log(this.name);
         this.getTechnologyByName(this.name);
-        console.log(this.name);
       }
     );
 
@@ -38,50 +37,51 @@ export class EditTechnologyComponent implements OnInit {
       category: ['', Validators.required],
       status: ['', Validators.required],
       description: ['', [Validators.required, Validators.minLength(10)]],
-      statusDescription: ['',],
+      statusDescription: [],
       author: ['', [Validators.required, Validators.minLength(3)]],
       created: [''],
       published: [''],
     });
   }
 
-  getCurrentPage(){
-    console.log(this.name);
-  }
+
 
   getTechnologyByName(name: string): void {
-    this.techDataService.getByName(this.name).subscribe(data => {
-      this.tech = data;
-      this.editTechForm.get('name')?.setValue(this.tech.name);
-      this.editTechForm.get('category')?.setValue(this.tech.category);
-      this.editTechForm.get('status')?.setValue(this.tech.status);
-      this.editTechForm.get('description')?.setValue(this.tech.description);
-      this.editTechForm.get('statusDescription')?.setValue(this.tech.statusDescription);
-      this.editTechForm.get('author')?.setValue(this.tech.author);
-      this.editTechForm.get('published')?.setValue(this.tech.published);
+    this.techDataService.getByName(this.name).subscribe({
+      next: (data) => {
+        this.tech = data;
+        this.editTechForm.get('name')?.setValue(this.tech.name);
+        this.editTechForm.get('category')?.setValue(this.tech.category);
+        this.editTechForm.get('status')?.setValue(this.tech.status);
+        this.editTechForm.get('description')?.setValue(this.tech.description);
+        this.editTechForm.get('statusDescription')?.setValue(this.tech.statusDescription);
+        this.editTechForm.get('author')?.setValue(this.tech.author);
+        this.editTechForm.get('published')?.setValue(this.tech.published);
+      },
+      error: (e) => {
+        if (e instanceof HttpErrorResponse && e.status === 401) {
+          this.router.navigate(['login']);
+        }
+      }
+
     });
   }
 
   onSubmit() {
-
-    console.log('Valid?', this.editTechForm.valid);
+    this.statusChangeCheck();
     if (this.editTechForm!.invalid) {
       return;
     }
-
     this.submitted = true;
     var published = this.editTechForm.value.published === true ? true : false;
-    console.log('Published', published);
     var date = new Date();
     var dateTimeStamp = date.toLocaleDateString() + " " + date.toLocaleTimeString();
     var newHistory: Change[] = [];
 
     if (!(typeof this.tech.history == 'undefined')) {
-      console.log("is not undefined")
-      console.log(this.tech.history)
       newHistory = this.tech.history!;
       newHistory.push({
-        author: this.tech.author!,
+        author: this.editTechForm.value.author,
         created: dateTimeStamp,
       })
     } else {
@@ -102,13 +102,18 @@ export class EditTechnologyComponent implements OnInit {
       published: published,
       history: newHistory,
     }
-
-    console.log(newObject);
-    this.techDataService.editTechnology(newObject)
+    this.techDataService.editTechnology(newObject, this.tech.name!)
     this.router.navigate(['']);
   }
   get getControl() {
     return this.editTechForm.controls;
   }
 
+  statusChangeCheck(): boolean {
+    if (!(this.tech.status! == this.editTechForm.value.status)) {
+      this.getControl["statusDescription"].setValidators(Validators.required);
+      this.getControl['statusDescription'].updateValueAndValidity()
+    }
+    return (this.tech.status! == this.editTechForm.value.status);
+  }
 }
