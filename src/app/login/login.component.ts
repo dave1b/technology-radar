d7e7600a617d57549250896e142871ad90bc50ac
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-03-08 09:23:03 +0100

    code indented

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { AuthService } from '../auth.service';
-
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm!: FormGroup;
   submitted: boolean = false;
-  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
+  errorMessage?: string;
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required]],
       pwd: ['', Validators.required],
-  
     });
   }
 
@@ -27,40 +33,23 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
-
-    // stop here if form is invalid
-    //console.log('Valid?', this.loginForm.valid); // true or false
     if (this.loginForm.invalid) {
       return;
     }
     this.submitted = true;
     var email = this.loginForm.value.email;
     var pwd = this.loginForm.value.pwd;
-    this.authService.loginUser({email: email, pwd: pwd}).subscribe(
-      res => {
-        localStorage.setItem('token',res.token)
-        console.log(res)
+    this.authService.loginUser({ email: email, pwd: pwd }).subscribe(
+      (res) => {
+        localStorage.setItem('token', res.token);
       },
-      err => {
-        console.log(err)
+      (err) => {
+        this.errorMessage = err.error;
       }
-      
-    )
-    /*
-    console.log('Name', this.adTechForm.value.name);
-    console.log('Category', this.adTechForm.value.category);
-    console.log('Status', this.adTechForm.value.status);
-    console.log('description', fothis.adTechFormrm.value.description); 
-    console.log('statusDescription', this.adTechForm.value.statusDescription); 
-    console.log('author', this.adTechForm.value.author); 
-    console.log('created', this.adTechForm.value.created); 
-    */
-    //this.techDataService.addNewTechnology(newObject);
-    //this.router.navigate(['']);
+    );
   }
 
   get getControl() {
     return this.loginForm!.controls;
   }
-
 }
